4939149f0475876b8cdbafe959b7424d938e09b6
EAJGit <allvaldi@outlook.com>
2025-02-02 16:37:57 +0000

    delete

---
+++ python/practice.py
@@ -0,0 +1,12 @@
+print('Pick a number')
+number1 = input()
+print('Pick another number')
+number2 = input()
+print('Pick your last number')
+number3 = input()
+combine = (int(number1) + int(number2) + int(number3))
+print(combine)
+
+#print('Whats is your name?')
+#name = input()
+#print('Hello', name)
---
+++ python/rom.py
@@ -0,0 +1,191 @@
+import random
+
+low = 1
+high = 100
+dice = random.randint(low, high)
+#origin of character
+
+if dice <31:
+    print ('Altered Human')
+elif dice >30 and dice <61:
+    print('Mutant')
+elif dice >60 and dice <91:
+    print('Hi-Tech')
+elif dice  >90 and dice <96:
+    print('Robot')
+else:
+    print('Alien')
+
+# Stats for Altered Human and Mutant
+
+if dice >0 and  dice <60:
+   dice = random.randint(low, high)
+   if dice >0 and dice <6:
+       print('Fighting:FB')
+   elif dice >5 and dice <11:
+       print('Figthing:PR')
+   elif dice >10 and dice <21:
+       print('Fighting:TY')
+   elif dice >20 and dice <41:
+        print('Fighting:GD')
+   elif dice >40 and dice <61:
+        print('Fighting:EX')
+   elif dice >60 and dice <81:
+        print('Fighting: RM')
+   elif dice >80 and dice <97:
+        print('Fighting:IN')
+   else:
+        print('Fighting')
+
+
+
+   if dice >0 and  dice <60:
+       dice = random.randint(low, high)
+   if dice >0 and dice <6:
+       print('Agility:FB')
+   elif dice >5 and dice <11:
+       print('Agility:PR')
+   elif dice >10 and dice <21:
+       print('Agility:TY')
+   elif dice >20 and dice <41:
+        print('Agility:GD')
+   elif dice >40 and dice <61:
+        print('Agility:EX')
+   elif dice >60 and dice <81:
+        print('Agility:RM')
+   elif dice >80 and dice <97:
+        print('Agility:IN')
+   else:
+        print('Agility:AM')
+
+
+
+   if dice >0 and  dice <60:
+    dice = random.randint(low, high)
+   if dice >0 and dice <6:
+       print('Strength:FB')
+   elif dice >5 and dice <11:
+       print('Strength:PR')
+   elif dice >10 and dice <21:
+       print('Strength:TY')
+   elif dice >20 and dice <41:
+        print('Strength:GD')
+   elif dice >40 and dice <61:
+        print('Strength:EX')
+   elif dice >60 and dice <81:
+        print('Strength:RM')
+   elif dice >80 and dice <97:
+        print('Strength:IN')
+   else:
+        print('Strength:AM')
+
+
+
+   if dice >0 and  dice <60:
+    dice = random.randint(low, high)
+   if dice >0 and dice <6:
+       print('Endurance:FB')
+   elif dice >5 and dice <11:
+       print('Endurance:PR')
+   elif dice >10 and dice <21:
+       print('Endurance:TY')
+   elif dice >20 and dice <41:
+        print('Endurance:GD')
+   elif dice >40 and dice <61:
+        print('Endurance:EX')
+   elif dice >60 and dice <81:
+        print('Endurance: RM')
+   elif dice >80 and dice <97:
+        print('Endurance:IN')
+   else:
+        print('Endurance:AM')
+
+  
+
+   if dice >0 and  dice <60:
+    dice = random.randint(low, high)
+   if dice >0 and dice <6:
+       print('Reason:FB')
+   elif dice >5 and dice <11:
+       print('Reason:PR')
+   elif dice >10 and dice <21:
+       print('Reason:TY')
+   elif dice >20 and dice <41:
+        print('Reason:GD')
+   elif dice >40 and dice <61:
+        print('Reason:EX')
+   elif dice >60 and dice <81:
+        print('Reason:RM')
+   elif dice >80 and dice <97:
+        print('Reason:IN')
+   else:
+        print('Reason:AM')
+
+
+
+   if dice >0 and  dice <60:
+    dice = random.randint(low, high)
+   if dice >0 and dice <6:
+       print('Intution:FB')
+   elif dice >5 and dice <11:
+       print('Intution:PR')
+   elif dice >10 and dice <21:
+       print('Intution:TY')
+   elif dice >20 and dice <41:
+        print('Intution:GD')
+   elif dice >40 and dice <61:
+        print('Intution:EX')
+   elif dice >60 and dice <81:
+        print('Intution:RM')
+   elif dice >80 and dice <97:
+        print('Intution:IN')
+   else:
+        print('Intution:AM')
+
+
+
+   if dice >0 and  dice <60:
+    dice = random.randint(low, high)
+   if dice >0 and dice <6:
+       print('Psyche:FB')
+   elif dice >5 and dice <11:
+       print('Psyche:PR')
+   elif dice >10 and dice <21:
+       print('Psyche:TY')
+   elif dice >20 and dice <41:
+        print('Psyche:GD')
+   elif dice >40 and dice <61:
+        print('Psyche:EX')
+   elif dice >60 and dice <81:
+        print('Psyche:RM')
+   elif dice >80 and dice <97:
+        print('Psyche:IN')
+   else:
+        print('Psyche:AM')
+
+
+#Realms of magic origin
+if dice <= 10:
+    print ('Magically Enhanced')
+elif dice >10 and  dice <36:
+    print ('Posseses Magical Items')
+else:
+    print('Magic Wielder')
+
+if dice<=10:
+    print('Your character is an altered human')
+  
+#Posseses magical items next step
+
+if dice >10 and  dice <36:
+   dice = random.randint(low, high)
+   if dice <=10 :
+        print('You have 1 item')
+   elif dice >10 and dice <51:
+         print ('You have 2 item/s')
+   elif dice > 50 and dice <91:
+         print ('You have 3 item/s')
+   else:
+    print('You have 4 item/s')
+
+
